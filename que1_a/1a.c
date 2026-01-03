4850467ae42d178d6ec096fba452e456b9b93ead
Naor Balas <naor56564@walla.com>
2026-01-03 12:07:05 +0200

    main 1a

--- que1_a/1a.c
+++ que1_a/1a.c
@@ -1,51 +1,12 @@
-#include <stdio.h>
-//#include <stdbool.h>
-//#include <string.h>
-#include <stdlib.h>
+#include "1a.h"
 
-size_t string_size(char str[]){
-    size_t actual_size_str=0;
-    while(str[actual_size_str]!='\0'){
-        actual_size_str++;
-    }
-    return actual_size_str;
-}
-
-int error_warning(size_t str1_len,size_t str2_len,size_t rough_size_str1){
-    if(rough_size_str1==0){
-        printf("%s\n","Error\nStr1 must be greater then 0");
-        return 1;
-    }
-    else if((str1_len+str2_len)>rough_size_str1){
-        printf("%s\n","Error\nStr1 does not have enoguh place to concatenate str2 with it.");   
-        return 1;
-    }
-   
-    return 0;
-}
-
-void my_strcat(char str1[],char str2[],size_t rough_size_str1){
-    size_t str1_len=string_size(str1);
-    size_t str2_len=string_size(str2);
-    if(error_warning(str1_len,str2_len,rough_size_str1)==1){
-        exit(EXIT_FAILURE);
-    }
-    for(size_t i=str1_len,j=0;i<(str1_len+str2_len)&&j<str2_len;i++,j++){
-        str1[i]=str2[j];
-    }
-}
 int main(void) {
     //code
     //allow watching in debuging
     setbuf(stdout, NULL);
-    char str1[100]="ma kore";
-    char str2[]=" koksinel";
+    char str1[20] = "Hello";
+    char str2[] = "WorldWorldWorldWorld";
     size_t rough_size_str1=sizeof(str1)/sizeof(str1[0]);
-    //size_t rough_size_str1=0;
-
-   
-    //printf("%s",str1);
-
     my_strcat(str1,str2,rough_size_str1);
 
     printf("%s\n",str1);
