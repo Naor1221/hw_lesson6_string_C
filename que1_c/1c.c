f71a0c6515958c9708721c3576aed2621b0c3632
Naor Balas <naor56564@walla.com>
2026-01-03 21:09:10 +0200

    que 1c

---
+++ que1_c/1c.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+void my_strncpy(char s1[],char s2[],size_t s1_len,size_t s2_size,size_t n){
+    if(s1_len>s2_size){
+        printf("%s","Error\nStr1 can't be greater than str2\n");
+    }
+    else if(n>s1_len){
+        n=s1_len;
+    }
+    for(size_t i=0;i<n;i++){
+        s2[i]=s1[i];
+    }
+    s2[n]='\0';
+}
+
+int main(void) {
+    //code
+    //allow watching in debuging
+    setbuf(stdout, NULL);
+    char s1[] = "Hello, Geeks!";
+    char s2[50];
+    size_t str1_len=sizeof(s1)/sizeof(s1[0]);
+    size_t str2_size=sizeof(s2)/sizeof(s2[0]);
+    
+    my_strncpy(s1,s2,str1_len,str2_size,50);
+    printf("%s\n",s2);
+
+    return 0;
+}
