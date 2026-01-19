648f4a147dc87f2984a1bce8a259334dace7e0f3
Naor Balas <naor56564@walla.com>
2026-01-19 12:28:19 +0200

    shorten syntex to idx=idx-len

--- que_8/8.c
+++ que_8/8.c
@@ -7,8 +7,8 @@ int main(void) {
     //code
     //allow watching in debuging
     setbuf(stdout, NULL);
-    char string[]="abc";
-    int n,len=(int)strlen(string);
+    char string[]="abcd";
+    int idx,len=(int)strlen(string);
     if(len==0){
         exit(EXIT_FAILURE);
     }
@@ -17,21 +17,23 @@ int main(void) {
     printf("Enter an index between %d to %d: ",len*2-1,len*(-1));
     //but this function exhibits different behaviour as the question
     //asked. this is way, the range of indexes it recieves is wider.
-    scanf("%d",&n);
-    if ( !( n<=(len*2 -1)&&n>=(len*(-1)) ) ){
+    scanf("%d",&idx);
+    if ( !( idx<=(len*2 -1)&&idx>=(len*(-1)) ) ){
         printf("%s\n","Index error");
         exit(EXIT_FAILURE);
     }
-    if( (n>0) && (n>=len) ){
-        n=n-len;
-        printf("%c\n",*(string+n));
+    if( (idx>0) && (idx>=len) ){
+        //idx=idx-len;
+        idx-=len;
+
+        printf("%c\n",*(string+idx));
     }
-    else if(n<0){
-        n*=-1;
-        printf("%c\n",*(string+len-n));
+    else if(idx<0){
+        idx*=-1;
+        printf("%c\n",*(string+len-idx));
     }
     else{
-        printf("%c\n",*(string+n));
+        printf("%c\n",*(string+idx));
     }
     return 0;
 }
