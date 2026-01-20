4690c90d588efd8208eab27c78ece406a3069eb2
Naor Balas <naor56564@walla.com>
2026-01-20 02:40:18 +0200

    que_9

---
+++ que_9/9a.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int is_palindrom_upgrade(char *ptr,size_t sz){
+    for(size_t i=0,j=sz-1;i<j;i++,j--){
+        if(*(ptr+i)!=*(ptr+j)){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(void) {
+    //code
+    //allow watching in debuging
+    setbuf(stdout, NULL);
+    char ptr[]="12321";
+    size_t len=0;
+    while(ptr[len]!='\0'){
+        len++;
+    }
+    if(is_palindrom_upgrade(ptr,len)==1){
+        printf("The array is palindrom\n");
+    }
+    else{
+        printf("The array is not palindrom\n");
+    }
+    return 0;
+}
---
+++ que_9/9b.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+int check(char c){
+    if( !(((c>=65)&&(c<=90)) || ((c>=97)&&(c<=122)) || ((c>=48)&&(c<=57))) ){
+        return 0;
+    }
+    return 1;
+}
+
+int is_palindrom_upgrade(char *ptr,size_t sz){
+    if(ptr==NULL){
+        return -1;
+    }
+    for(size_t i=0,j=sz-1;i<j;i++,j--){
+        if(check(*(ptr+i))==0){
+            while(check(ptr[++i])==0){}
+        }
+        if (check(*(ptr+j))==0){
+            while(check(ptr[--j])==0){}
+        }
+        if(*(ptr+i)!=*(ptr+j)){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main(void) {
+    //code
+    //allow watching in debuging
+    setbuf(stdout, NULL);
+    char ptr[]="12 3 4 9894'! 3 2 1";
+    size_t len=0;
+    while(ptr[len]!='\0'){
+        len++;
+    }
+    if(is_palindrom_upgrade(ptr,len)==1){
+        printf("The array is palindrom\n");
+    }
+    else{
+        printf("The array is not palindrom\n");
+    }
+    return 0;
+}
